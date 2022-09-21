41afd65539725b75254a3a186b3e17d44490d120
Noureddine-0 <114105927+Noureddine-0@users.noreply.github.com>
2022-09-21 19:18:56 +0000

    Update transformation.py

--- transformation.py
+++ transformation.py
@@ -1,4 +1,6 @@
-### Solution for Transformation ###
+''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag), 2)])
+
+# the purpose of the following script is to reverse engineering what was written above. 
 
 a=input('Enter your phrase:')
 s=''
