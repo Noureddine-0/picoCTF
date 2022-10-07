d9c7443f2616f9c3152929c78f27630ded592359
Noureddine-0 <114105927+Noureddine-0@users.noreply.github.com>
2022-10-07 13:51:33 +0000

    Create play_nice.py

---
+++ play_nice.py
@@ -0,0 +1,32 @@
+alphabet:'0uxtb3w4kj26q9m8gioe7nvahplr5dy1fzcs'
+SQUARE_SIZE = 6
+def generate_square(alphabet):
+	assert len(alphabet) == pow(SQUARE_SIZE, 2)   
+	matrix = []             
+  for i, letter in enumerate(alphabet):  
+		if i % SQUARE_SIZE == 0:  
+			row = []
+		row.append(letter)
+		if i % SQUARE_SIZE == (SQUARE_SIZE - 1):
+			matrix.append(row)
+	return matrix
+
+def decrypt_pair(pair,matrix):
+	p1 = get_index(pair[0],matrix)
+	p2 = get_index(pair[1],matrix)
+	if p1[0]==p2[0]:
+		return matrix[p1[0]][(p1[1] - 1)  % SQUARE_SIZE] + matrix[p2[0]][(p2[1] - 1)  % SQUARE_SIZE]
+	elif p1[1]==p2[1]:
+		return matrix[(p1[0] - 1)  % SQUARE_SIZE][p1[1]] + matrix[(p2[0] - 1)  % SQUARE_SIZE][p2[1]]
+	else:
+		return matrix[p1[0]][p2[1]] + matrix[p2[0]][p1[1]]
+
+def decrypt_message(message,matrix):
+	result=""
+	for i in range(0, len(message), 2):
+		result += decrypt_pair(message[i:i + 2], matrix)
+	return result
+
+print(decrypt_message('xj5c181ropf5xjmyujnv0wlqrjdrbz',generate_square(alphabet)))
+
+#the flag is not in standard format , so just paste the output without picoCTF
